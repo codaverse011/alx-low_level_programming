51d36d088d64b12b6dfcc865b226d53933f6d97c
codaverse011 <thandimtema@gmail.com>
2022-06-02 20:31:53 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 
+
 /**
  * main - Entry point
  *
@@ -8,6 +9,6 @@
 int main(void)
 {
 	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2,str1,159);
+	write(2,str1,59);
 	return (1);
 }
