fd3e37f0bfb6db3ffe23737cb281c37af37d0cec
codaverse011 <thandimtema@gmail.com>
2022-06-05 11:45:28 -0700

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 102; i < 97; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
