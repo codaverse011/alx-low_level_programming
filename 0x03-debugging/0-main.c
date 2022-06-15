cdff828ea38811a765f2056ccedd8185a814a71c
codaverse011 <ngollekaopt@gmail.com>
2022-06-15 00:14:57 -0700

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if int is positive or negative
+ * Return: zero
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
