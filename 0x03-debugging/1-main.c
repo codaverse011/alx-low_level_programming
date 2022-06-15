dc6a7f3ecd901be978629584e9d474165333f129
codaverse011 <ngollekaopt@gmail.com>
2022-06-15 00:46:06 -0700

    task01

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,6 +9,7 @@ int main(void)
 	int i;
 
 	printf("infinite loop incoming :(\n");
+
 	i = 0;
 
 	/*
@@ -17,6 +18,6 @@ int main(void)
 	 * putchar(i);
 	 * }
 	 */
-	printf("infinite loop avoiided! \\o/\n");
+	printf("infinite loop avoided! \\o/\n");
 	return (0);
 }
