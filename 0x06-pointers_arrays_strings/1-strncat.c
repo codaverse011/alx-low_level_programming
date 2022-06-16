a8aeecccbc354311217c0523d36da42c57643109
codaverse011 <ngollekaopt@gmail.com>
2022-06-16 03:09:57 -0700

    task1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strncat - cancatenates two strings
+ * @dest: pointer
+ * @src: pointer
+ * @n: integer
+ *
+ * Return: void
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a = 0;
+	int b = 0;
+
+	while (dest[a] != '\0')
+		a++;
+	while (src[b] != '\0' && b < n)
+	{
+		dest[a] = src[b];
+		b++;
+		a++;
+	}
+	return (dest);
+}
