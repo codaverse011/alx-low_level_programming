933491d8446e4a7759d9aeb9e5e9b6e7bd04db62
codaverse011 <ngollekaopt@gmail.com>
2022-06-16 02:50:46 -0700

    task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,25 +1,24 @@
 #include "main.h"
 
 /**
- *_strncat - function that concatenates two strings. it will use
- * at most n bytes from src. src does not need to be null terminated.
- *@dest: pointer to destination string.
- *@src: pointer to source string.
- *@n: number of bytes to be concatenated.
+ *_strncat - function that concatenates two numbers
+ *@dest: parameter to append
+ *@src: parameter to source
  *
- *Return: pointer to destination string.
+ *Return: Always 0
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strcat(char *dest, char *src,)
 {
-	int c, c2;
-	/*find the size of the dest array*/
-
-	while (dest[c])
-		c++;
-	/*iterate through each src array value without the null byte*/
+	int a = 0;
+	int b = 0;
 	
-	for (c2 = 0; src[c2]; c++)
-	/*append src[c2] to dest[c] while overwritting the null byte in dest*/
-	dest[c++] = src [c2];
+	while (dest[a] != '\0')
+		a++;
+	while (src[b] != '\0')
+	{
+		dest[a] = src[b];
+		b++;
+		a++;
+	}
 	return (dest);
 }
