eda2b01578f051d600711f3888373f7473e8984e
codaverse011 <ngollekaopt@gmail.com>
2022-06-16 07:05:21 -0700

    trial

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <string.h>
-#include <stdio.h>
 
 /**
  *_strncat - function that concatenates two numbers
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,8 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+int _putchar(int c);
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
+/**char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
@@ -11,6 +13,6 @@ char *cap_string(char *);
 char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, *n2, char *r, int size_r);
-void print_buffer(char *b, int size);
-int _putchar(int c);
+void print_buffer(char *b, int size);*/
+
 #endif
