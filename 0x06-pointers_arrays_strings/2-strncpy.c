4684250cc5cbf1a9bfdca825f89516682da87dad
codaverse011 <ngollekaopt@gmail.com>
2022-06-16 07:38:17 -0700

    task2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strncpy - concatenates two string
+ * @dest: Destination pointer
+ * @src: Source pointer string
+ * @n: String size
+ * Return: Return a concatenated string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
