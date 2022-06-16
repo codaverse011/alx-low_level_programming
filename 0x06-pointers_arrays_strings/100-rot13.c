7d9cb87e9ac6dcaeca8a6a6d438116b542ab0d0b
codaverse011 <ngollekaopt@gmail.com>
2022-06-16 11:13:03 -0700

    100rot

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *rot13 - changes all letters of a string to leet.
+ *@c: input characters
+ *Return: Pointer to characters
+ */
+char *rot13(char *c)
+{
+	char *alphas = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *codes = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int i = 0;
+	
+	while (c[i] != '\0')
+	{
+		int j = 0;
+		
+		while (alphas[j] != '\0')
+		{
+			if (c[i] == alphas[j])
+			{
+				c[i] = codes[j];
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (c);
+}
