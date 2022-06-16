94b93f6eb890d43236af6be0674b2c25598e3d62
codaverse011 <ngollekaopt@gmail.com>
2022-06-16 11:33:10 -0700

    101-print

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *print_number - prints a number
+ *@n: number
+ *
+ *Return: void
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	else
+	{
+		int x = n;
+		n = n / 10;
+		if (n > 0)
+		{
+			print_number(n);
+		}
+		_putchar((x % 10) + 48);
+	}
+}
