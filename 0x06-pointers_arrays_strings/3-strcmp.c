a6bc8037f6a5df125b3bbe12e10bb0412acb2e75
codaverse011 <ngollekaopt@gmail.com>
2022-06-16 07:47:12 -0700

    Task3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcmp - Function that compares two strings
+ * @s1: Pointer to null-terminated first string
+ * @s2: Pointer to null-terminated second string
+ *
+ * Return: 0 (success)
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
